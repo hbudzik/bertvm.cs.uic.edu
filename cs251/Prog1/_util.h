3083b1806c7dc466eaf7f9b86cb4ce483a4b67c9
Hubert Budzik <hubertbudmobile@gmail.com>
2018-02-13 23:01:53 -0600

    rest test cases for prog 1

--- cs251/Prog1/_util.h
+++ cs251/Prog1/_util.h
@@ -66,6 +66,25 @@ bool eq2array(List<int> *lst, int a[], int n) {
   return ret;
 
 }
+/**
+void parray(int a[], int n) {
+int i;
+
+  printf("    [ ");
+  for(i=0; i<n; i++) {
+    printf(" %i ", a[i]);
+  }
+  printf("]\n");
+}
+**/
+
+int eq2arrayP(List<int> *lst, int a[], int n) {
+  printf("      Expected Contents:  ");
+  parray(a, n);
+  printf("      Your List        :  ");
+  lst->print();
+  return eq2array(lst, a, n);
+}
 
 
 List<int> * arr2list(int a[], int n) {
@@ -130,5 +149,111 @@ bool lsts_equal_destructive(List<int> *a, List<int> *b) {
   return ret;
 } 
 
+/**********************************************/
+
+/**
+int num_occ(int a[], int n, int x) {
+int matches=0;
+int i;
+
+  for(i=0; i<n; i++) {
+	if(a[i]==x) matches++;
+  }
+  return matches;
+}
+
+int same_contents_arr(int a[], int b[], int n) {
+int i, x;
+
+  for(i=0; i<n; i++) {
+	x = a[i];
+	if(num_occ(a, n, x) != num_occ(b, n, x))
+		return 0;
+  }
+  return 1;
+}
+**/
+
+
+
+int same_contents(List<int> *a, List<int> *target, int print) {
+  int n, i;
+
+  if(print) {
+    printf("        Expected Contents:  ");
+    target->print();
+    printf("        Actual Contents  :  ");
+    a->print();
+  }
+
+  n = a->length();
+  if(n != target->length()) return 0;
+
+  int aa[n];
+  int t[n];
+  for(i=0; i<n; i++){
+    a->pop_front(aa[i]);
+    target->pop_front(t[i]);
+  }
+
+  return same_contents_arr(aa, t, n);
+}
+  
+  
+
+List<int> * rem_all_hard_caseA(int nzeros) {
+  int i;
+
+  List<int> *lst =  new List<int>();
+
+  for(i=0; i<nzeros; i++) {
+    lst->push_front( 0);
+  }
+  for(i=0; i<nzeros; i++) {
+    lst->push_front( 1);
+  }
+  return lst;
+}
+
+  
+List<int> * rem_all_hard_caseB(int nzeros) {
+  int i;
+
+  List<int> *lst =  new List<int>();
+
+  for(i=0; i<nzeros/2; i++) {
+    lst->push_front( 0);
+  }
+  for(i=0; i<nzeros/2; i++) {
+    lst->push_front( 1);
+  }
+  for(i=0; i<nzeros-(nzeros/2); i++) {
+    lst->push_front( 0);
+  }
+  for(i=0; i<nzeros-(nzeros/2); i++) {
+    lst->push_front( 1);
+  }
+  return lst;
+}
+
+
+/*
+* expects list to contain only ones AND for
+*  there to be exactly n of them.
+* returns 0/1 accordingly
+*
+* Note:  empties the list
+*/
+int n_ones(List<int> *lst, int n) {
+  int cnt=0;
+  int x;
+
+  while(!lst->is_empty()) {
+    lst->pop_front(x);
+    if(x != 1) return 0;
+    cnt++;
+  }
+  return cnt==n;
+}
 
 #endif
